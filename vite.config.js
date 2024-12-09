81fbbd1660f8ebc0093ec470044613b6ed86f9d6
Pedrobks23 <100809090+Pedrobks23@users.noreply.github.com>
2024-12-09 10:48:25 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        /^@/assets/.*$/
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './assets')
+    }
+  }
 })
